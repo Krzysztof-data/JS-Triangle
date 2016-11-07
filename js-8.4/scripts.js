9edb4ee6e4cf963f9b850652285d66ac0a0bac26
Krzysztof Sadowski <k.sadowski@poczta.onet.pl>
2016-11-07 21:36:45 +0100

    js-8.4

---
+++ js-8.4/scripts.js
@@ -0,0 +1,10 @@
+var a;
+var h;
+var triangleArea;
+
+var a = prompt ('Podaj wartość a');
+var h = prompt ('Podaj wartość h');
+var triangleArea = a*h/2;
+
+console.log(triangleArea);
+document.write('Pole trójkąta o podstawie a: = ' + a + ' i wysokości h: = ' + h + ' wynosi: ' + triangleArea);
