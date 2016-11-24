5577da6df5b793f2c7af0abe8c32985b91bed440
Krzysztof Sadowski <k.sadowski@poczta.onet.pl>
2016-11-25 00:29:17 +0100

    chapter 8.9

--- scripts.js
+++ scripts.js
@@ -1,17 +1,11 @@
-function Telefon(marka, cena, kolor, system, wyświetlacz) { 
-  this.marka = marka; 
-  this.cena = cena; 
-  this.kolor = kolor;
-  this.system = system;
-  this.wyświetlacz = wyświetlacz;
-} 
-Telefon.prototype.printInfo = function() {
-  console.log('Marka telefonu to ' + this.marka + ', kolor to ' + this.kolor + ', a cena to ' + this.cena + ', system operacyjny ' + this.system + ', rodzaj wyświetlacza ' + this. wyświetlacz + '.'); 
+function rysujChoinkę(n) {
+  for (var i = 0; i < n; i++) {
+      var star = ' '; 
+      for (var s = 0; s < i * 2 - 1; s++){ 
+           star += '*';
+      }
+         console.log(star);
+  }
 }
-var iPhone6S = new Telefon('Apple', 2250, 'srebrny', 'iOS', 'IPS TFT');
-var samsungGalaxyS6 = new Telefon('Samsung', 1659, 'czarny', 'Android', 'super AMOLED');
-var onePlusOne = new Telefon('OnePlus One', 1299, 'czarny', 'Android', 'IPS TFT');
 
-iPhone6S.printInfo();
-samsungGalaxyS6.printInfo();
-onePlusOne.printInfo(); 
+rysujChoinkę(5);
