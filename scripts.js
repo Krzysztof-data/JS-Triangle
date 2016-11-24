51d0cf7c5492dd90c82b62981eb366a2481d36bf
Krzysztof Sadowski <k.sadowski@poczta.onet.pl>
2016-11-24 23:43:13 +0100

    chapter 10.4

--- scripts.js
+++ scripts.js
@@ -1,14 +1,17 @@
-var url = ('https://api.icndb.com/jokes/random');
-var paragraph = document.getElementById('joke');
-var button = document.getElementById('get-joke');
-button.addEventListener('click', function(){
-	var xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.addEventListener('load', function(){
-    var response = JSON.parse(xhr.response);
-    paragraph.innerHTML = response.value.joke;
-  });
-  xhr.send();
-});
- 
+function Telefon(marka, cena, kolor, system, wyświetlacz) { 
+  this.marka = marka; 
+  this.cena = cena; 
+  this.kolor = kolor;
+  this.system = system;
+  this.wyświetlacz = wyświetlacz;
+} 
+Telefon.prototype.printInfo = function() {
+  console.log('Marka telefonu to ' + this.marka + ', kolor to ' + this.kolor + ', a cena to ' + this.cena + ', system operacyjny ' + this.system + ', rodzaj wyświetlacza ' + this. wyświetlacz + '.'); 
+}
+var iPhone6S = new Telefon('Apple', 2250, 'srebrny', 'iOS', 'IPS TFT');
+var samsungGalaxyS6 = new Telefon('Samsung', 1659, 'czarny', 'Android', 'super AMOLED');
+var onePlusOne = new Telefon('OnePlus One', 1299, 'czarny', 'Android', 'IPS TFT');
 
+iPhone6S.printInfo();
+samsungGalaxyS6.printInfo();
+onePlusOne.printInfo(); 
