aa509f873ef69d440fafa36b0543550e0583eeb7
Krzysztof Sadowski <k.sadowski@poczta.onet.pl>
2016-11-26 14:51:00 +0100

    chapter 10.5

--- scripts.js
+++ scripts.js
@@ -1,11 +1,19 @@
-function rysujChoinkę(n) {
-  for (var i = 0; i < n; i++) {
-      var star = ' '; 
-      for (var s = 0; s < i * 2 - 1; s++){ 
-           star += '*';
-      }
-         console.log(star);
+function Button(text) {
+		this.text = text || 'Hello';
+}
+Button.prototype = {
+	create: function(){
+  	var self = this;
+  	this.$element = $('<button>');
+    this.$element.text(this.text);
+    this.$element.click(function(){
+    		alert(self.text);
+    });
+   
+     $('body').append(this.$element);
+              
   }
 }
+var btn1 = new Button('Hello');
 
-rysujChoinkę(5);
+btn1.create();
