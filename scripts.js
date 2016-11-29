1ad5fdba709387323620a29ccd5420aaefebd462
Krzysztof Sadowski <k.sadowski@poczta.onet.pl>
2016-11-29 01:18:46 +0100

    JS chapter  8.11

--- scripts.js
+++ scripts.js
@@ -1,14 +1,8 @@
-function rysujChoinkę(j) {
-  for(i = 1; i <= j; i++) {
-    var star = '';
-    for(n = 0; (i * 2 - 1) > n; n++) {
-      star += '*';
-      }
-  for(s = j; s - i >= 1;  s--) {
-    star = ' '  + star;
-        }
-    console.log(star);
-    }
-}
-rysujChoinkę(5);
+var list = document.getElementById('Lista'),
+	add = document.getElementById('addElem'),
+	itemsByTagName = document.getElementsByTagName('li');
+
+add.addEventListener('click', function() {
+	list.innerHTML += '<li>item</li>' + itemsByTagName.length
+});
 
