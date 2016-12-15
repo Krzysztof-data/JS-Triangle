a1c9993c1035972273e041fabd02b6b502946805
Krzysztof Sadowski <k.sadowski@poczta.onet.pl>
2016-12-16 00:51:28 +0100

    chapter 10.6

--- scripts.js
+++ scripts.js
@@ -1,25 +1,142 @@
-var tweetLink = "https://twitter.com/intent/tweet?text=";
-var quoteUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&key=867576&format=jsonp&lang=en&jsonp=?";
-
-function getQuote() {
-    $.getJSON(quoteUrl, createTweet);
-}
-function createTweet(input) {
-    if (!input.quoteAuthor.length) {
-        input.quoteAuthor = "Unknown author";
-      }
-      var tweetText = "Quote of the day - " + input.quoteText + " Author: " + input.quoteAuthor;
-        if (tweetText.length > 140) {
-            getQuote();
-    } else {
-        var tweet = tweetLink + encodeURIComponent(tweetText);
-            $('.quote').text(input.quoteText);
-            $('.author').text("Author: " + input.quoteAuthor);
-            $('.tweet').attr('href', tweet);
+function randomString() {
+    var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
+    var str = '';
+    var i = 0;
+    for (i = 0; i < 10; i++) {
+        str += chars[Math.floor(Math.random() * chars.length)];
     }
- };
- $(document).ready(function() {
-    getQuote(); $('.trigger').click(function() {
-        getQuote();
-   })
+    return str;
+};
+
+function Column(name) {
+    var self = this;
+
+    this.id = randomString();
+    this.name = name;
+    this.$element = createColumn();
+
+    function createColumn() {
+        var $column = $('<div>').addClass('column');
+        var $columnTitle = $('<h2>').addClass('column-title').text(self.name);
+        var $columnCardList = $('<ul>').addClass('column-card-list');
+        var $columnDelete = $('<button>').addClass('btn-delete').text('x');
+        var $columnAddCard = $('<button>').addClass('add-card').text('Dodaj kartę');
+
+        $columnDelete.click(function() {
+            var r = confirm("Usunąć kolumnę?");
+            if (r == true) {
+                self.removeColumn();
+            } else {
+                console.log("Anuluj!");
+            }
+        });
+
+        $columnAddCard.click(function() {
+
+            var newpro = prompt("Wpisz nazwę karty")
+            if (newpro == ''){
+                alert('Karta musi mieć nazwę')
+            } else if (newpro == null){
+                undefined;
+            }
+            else {
+                self.addCard(new Card(newpro));
+            }
+        });
+
+        $column.append($columnTitle)
+            .append($columnDelete)
+            .append($columnAddCard)
+            .append($columnCardList);
+        return $column;
+    };
+};
+
+Column.prototype = {
+    addCard: function(card) {
+        this.$element.children('ul').append(card.$element);
+    },
+    removeColumn: function() {
+        this.$element.remove();
+    }
+};
+
+function Card(description) {
+    var self = this;
+
+    this.id = randomString();
+    this.description = description;
+    this.$element = createCard();
+
+    function createCard() {
+        var $card = $('<li>').addClass('card');
+        var $cardDescription = $('<p>').addClass('card-description').text(self.description);
+        var $cardDelete = $('<button>').addClass('btn-delete').text('x');
+
+        $cardDelete.click(function(){
+            var r = confirm("Usunąć kartę?");
+            if (r == true) {
+                self.removeCard();
+            } else {
+                undefined;
+            }
+        });
+
+        $card.append($cardDelete)
+            .append($cardDescription);
+
+        return $card;
+    }
+};
+
+Card.prototype = {
+    removeCard: function() {
+        this.$element.remove();
+    }
+};
+
+var board = {
+    name: 'Tablica Kanban',
+    addColumn: function(column) {
+        this.$element.append(column.$element);
+        initSortable();
+    },
+    $element: $('#board .column-container')
+};
+
+function initSortable() {
+    $('.column-card-list').sortable({
+        connectWith: '.column-card-list',
+        placeholder: 'card-placeholder'
+    }).disableSelection();
+};
+
+$('.create-column')
+    .click(function(){
+    var name = prompt('Wpisz nazwę kolumny');
+    var column = new Column(name);
+    var containerWidth = $('.container').width();
+
+    if (name == '') {
+        alert('Kolumna musi mieć nazwę')
+    }   else if (name == null){
+        undefined;
+    }   else {
+        board.addColumn(column);
+        $('.container').css({"width":containerWidth+280});
+    }
 });
+
+var todoColumn = new Column('Do zrobienia');
+var doingColumn = new Column('W trakcie');
+var doneColumn = new Column('Skończone');
+
+board.addColumn(todoColumn);
+board.addColumn(doingColumn);
+board.addColumn(doneColumn);
+
+var card1 = new Card('Nowe zadanie');
+var card2 = new Card('Stworzyc tablice kanban');
+
+todoColumn.addCard(card1);
+doingColumn.addCard(card2);
